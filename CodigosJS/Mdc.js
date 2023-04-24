78b7fdbd04adb402a5a06d67accb1c565e5536c4
Isaac Rosendo <116417121+IsaacRL1@users.noreply.github.com>
2023-04-24 00:53:38 -0300

    V1.4

--- CodigosJS/Mdc.js
+++ CodigosJS/Mdc.js
@@ -1,15 +1,10 @@
-
 function calcularSomatorio() {
-    // Pega os valores dos campos de entrada
+    
     var inicio = parseInt(document.getElementById("inicio").value);
     var fim = parseInt(document.getElementById("fim").value);
-
-    // Calcula o somatório dos números no intervalo
     var soma = 0;
     for(var i = inicio; i <= fim; i++) {
         soma += i;
     }
-
-    // Mostra o resultado na página
     document.getElementById("resultado").innerHTML = "O somatório dos números no intervalo é " + soma;
 }
